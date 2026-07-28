7241db8555d0467fec9e02d26a1b521b57865cb1
HET MODI <modijignisha12@gmail.com>
2026-07-28 13:35:37 +0530

    Update Q3.c

--- LAB EXERCISE/Q3.c
+++ LAB EXERCISE/Q3.c
@@ -1,1 +1,25 @@
-.
+#include <stdio.h>
+int main()
+{
+	int a,b,sum,diff,prod,rem,quot1;
+	float quot2;
+	
+	printf("Enter your 1st number\n");
+	scanf("%d",&a);
+	printf("Enter your 2nd number\n");
+	scanf("%d",&b);
+	
+	sum=a+b;
+	diff=a-b;
+	prod=a*b;
+	quot1=a/b;
+	rem=a%b;
+	quot2=a%b;
+	
+	printf("sum is %d",sum);
+	printf("\ndiff is %d",diff);
+	printf("\nproduct is %d",prod);
+	printf("\nremainder is %d",rem);
+	printf("\nquotient is %d",quot1);
+	printf("\nquotient is %.2f",quot2);	
+}
