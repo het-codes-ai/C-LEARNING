4092009975db7ec8a656b65a007aec6f54c5c57d
HET MODI <modijignisha12@gmail.com>
2026-07-28 13:26:13 +0530

    Update Q2.c

--- LAB EXERCISE/Q2.c
+++ LAB EXERCISE/Q2.c
@@ -1,1 +1,8 @@
-.
+#include <stdio.h>
+int main(){
+	float celcius, farenheit;
+	printf("Enter your temperature:\n ");
+	scanf("%f",&celcius);
+	farenheit=(celcius*9.0/5.0)+32;
+	printf("Your temperature in farenheit =%f",farenheit) ;
+}
