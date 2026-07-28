39428efada2cbdf1192a8fbc8b7b10d1bbe48d7e
HET MODI <modijignisha12@gmail.com>
2026-07-28 13:18:58 +0530

    Update Q1.c

--- LAB EXERCISE/Q1.c
+++ LAB EXERCISE/Q1.c
@@ -1,1 +1,13 @@
-.
+#include <stdio.h>
+int main(){
+	int i;
+	float f;
+	char c;
+	double d;
+	
+	printf("SIZE OF int IS: %d\n",sizeof(i));
+	printf("SIZE OF float IS: %d\n",sizeof(f));
+	printf("SIZE OF char IS: %d\n",sizeof(c));
+	printf("SIZE OF double IS: %d\n",sizeof(d));
+	return 0;
+}
