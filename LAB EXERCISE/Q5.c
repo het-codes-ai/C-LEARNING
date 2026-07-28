8f26594b668caaf74fc660fea57cf3165c81ad0e
HET MODI <modijignisha12@gmail.com>
2026-07-28 07:00:30 +0530

    Create Q5.c

---
+++ LAB EXERCISE/Q5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+        int a,b,temp;
+        a=6;
+        b=9;
+        printf("before swap a=%d,b=%d \n ",a,b);
+        temp=a;
+        a=b;
+        b=temp;
+        printf("after swap a=%d,b=%d \n ",a,b);
+
+}
+
