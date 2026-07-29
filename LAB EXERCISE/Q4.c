28d3ccd3b8b4a6d7772c6b1589834af1907ceecd
HET MODI <modijignisha12@gmail.com>
2026-07-29 08:59:15 +0530

    Update Q4.c

--- LAB EXERCISE/Q4.c
+++ LAB EXERCISE/Q4.c
@@ -1,1 +1,16 @@
-.
+#include <stdio.h> 
+int main(){
+	float l,b,area,perimeter;
+	printf("Enter length: \n");
+	scanf("%f",&l);
+	printf("Enter breadth: \n");
+	scanf("%f",&b);
+	
+	area=l*b;
+	perimeter=2*(l+b);
+	
+	printf("Area is:%.2f",area);
+	printf("Perimeter is:%.2f",perimeter);
+	
+	return 0;
+}
