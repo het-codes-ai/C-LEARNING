c405114cd856c207a80ae1e4b8c054be22b5220c
HET MODI <modijignisha12@gmail.com>
2026-07-30 16:18:30 +0530

    Create Q8.c

---
+++ LAB EXERCISE/Q8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int a=50;
+	int b=3;
+	int div1=a/b;
+	
+	printf("INT DIV = %d\n",div1);
+	
+	float c=50.0;
+	int d=3;
+	float div2=c/d;
+	
+	printf("FLOAT DIV = %d\n",div2);
+	
+	}
