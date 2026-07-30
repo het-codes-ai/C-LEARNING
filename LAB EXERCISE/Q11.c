e84ade9f5da9e1a0bce969dfc418ea735cd7651a
HET MODI <modijignisha12@gmail.com>
2026-07-30 16:35:16 +0530

    Create Q11.c

---
+++ LAB EXERCISE/Q11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	float a=0.1f;
+	float b=0.1f;
+	float c=0.1f;
+	float d=0.1f;
+	float e=0.1f;
+	float f=0.1f;
+	float g=0.1f;
+	float h=0.1f;
+	float i=0.1f;
+	float j=0.1f;
+	float sum=a+b+c+d+e+f+g+h+i+j;
+	printf("%.1f\n",sum);
+	
+	sum==1.0?printf("True"):printf("False");
+	
+	return 0;
+	}
