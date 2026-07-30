e409cdbf0347afc6cfa05af69335eb3f44e0f67f
HET MODI <modijignisha12@gmail.com>
2026-07-30 07:18:32 +0530

    Create Q7.c

---
+++ LAB EXERCISE/Q7.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int amount,rupees,paise,note500,note100,note10,coin1;
+	printf("Enter your PAISE\n");
+	scanf("%d",&amount);
+	 
+	rupees=amount/100;
+	paise=amount%100;
+	
+	printf("Rs %d\n",rupees);
+	printf("Ps %d\n",paise);
+	
+	note500=rupees/500;
+	rupees%=500;
+	
+	note100=rupees/100;
+	rupees%=100;
+	
+	note10=rupees/10;
+	rupees%=10;
+	
+	coin1=rupees/1;
+	
+	printf("500 rs notes : %d\n",note500);
+	printf("100 rs notes : %d\n",note100);	
+	printf("10 rs notes : %d\n",note10);	
+	printf("1 rs coin : %d\n",coin1);
+	printf("Paise : %d\n",paise);
+	return 0;
+	}
