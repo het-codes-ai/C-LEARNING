7252fe7b49744414a463cd2f5c31d9eb31ac4a29
HET MODI <modijignisha12@gmail.com>
2026-08-04 16:02:48 +0530

    Update  1.c

--- MARATHON-1 / I-O EXPRESSION / 1.c
+++ MARATHON-1 / I-O EXPRESSION / 1.c
@@ -1,1 +1,10 @@
-.
+#include<stdio.h>
+int main(){
+	float dollars,rupees,converter;
+	printf("Enter money in dollars :\n");
+	scanf("%f",&dollars);
+	converter=95.37;
+	rupees=dollars*converter;
+	printf("Your dollars are converted to %.2f rupees",rupees);
+	return 0;
+	}
