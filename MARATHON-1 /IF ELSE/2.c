086e2f2c289e02d08cee87cb2003e0d7a45d369e
HET MODI <modijignisha12@gmail.com>
2026-08-05 06:32:56 +0530

    Create 2.c

---
+++ MARATHON-1 /IF ELSE/2.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+int main(){
+	int num;
+	printf("Enter your number \n");
+	scanf("%d",&num);
+	if(num%2==0){
+ 		printf("EVEN");
+ 				}
+	else{
+		printf("ODDA");
+		}
+	return 0;
+	}
