9dd879c65a579c94fd9f8c1bc23dbb14553dd993
HET MODI <modijignisha12@gmail.com>
2026-08-05 09:04:33 +0530

    Create 6.c

---
+++ MARATHON-1 /SWITCH/6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    switch(num > 0) {
+        case 1: 
+            printf("Positive\n"); 
+            break;
+        case 0: 
+            switch(num < 0) {
+                case 1: 
+                    printf("Negative\n"); 
+                    break;
+                case 0: 
+                    printf("Zero\n"); 
+                    break;
+            }
+            break;
+    }
+    return 0;
+}
