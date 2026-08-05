848861af31cf5b2bdec9fde4ec08c206b58f59b5
HET MODI <modijignisha12@gmail.com>
2026-08-05 06:21:14 +0530

    Update 1.c

--- MARATHON-1 /IF ELSE/1.c
+++ MARATHON-1 /IF ELSE/1.c
@@ -1,1 +1,16 @@
-.
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    if (number > 0) {
+        printf("%d is Positive.\n", number);
+    } else if (number < 0) {
+        printf("%d is Negative.\n", number);
+    } else {
+        printf("The number is Zero.\n");
+    }
+
+    return 0;
+}
