370b13fa47745b1b6321ba1a4c0bb5063ea24a1c
HET MODI <modijignisha12@gmail.com>
2026-08-06 18:18:15 +0530

    Create 9.c

---
+++ MARATHON-1 /WHILE/9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int n=20;
+	int i=1;
+	printf("Even numbers in range are :\n");
+	while(i<=n){
+		if(i%2==0){
+			printf("%d\n",i);		
+			}
+		i++;
+		}
+		return 0;
+	}
