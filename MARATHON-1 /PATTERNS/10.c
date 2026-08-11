62491abfd745c9c03cc7cb88f8cc399478cfdbff
HET MODI <modijignisha12@gmail.com>
2026-08-11 07:52:47 +0530

    Implement pattern printing with cubes
    Add a program to print a pattern of cubes.

--- MARATHON-1 /PATTERNS/10.c
+++ MARATHON-1 /PATTERNS/10.c
@@ -1,1 +1,15 @@
-.
+#include<stdio.h>
+int main(){
+    int i,sp,j;
+    for(i=1;i<=5;i++){
+        for(sp=1;sp<=5-i;sp++){
+            printf("_ ");
+        }
+        for(j=i;j>=1;j--){
+            printf("%d ",j*j*j);
+        }
+    printf("\n");
+    }
+
+return 0;
+}
